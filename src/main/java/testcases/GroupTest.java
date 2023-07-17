ff3e56e029f35e2aa2ecf1dc15274d457cf92aec
PriyankaPVibhandik <89004721+PriyankaPVibhandik@users.noreply.github.com>
2023-07-17 18:44:56 +0530

    Update GroupTest.java

--- src/main/java/testcases/GroupTest.java
+++ src/main/java/testcases/GroupTest.java
@@ -1,12 +1,13 @@
 package testcases;
 
+import Freshsales_Automation.testBase;
 import org.testng.annotations.Test;
 
 import Pages.LoginPage;
 import Pages.groupAndSortPage;
-import base.BaseTest;
+//import base.BaseTest;
 
-public class GroupTest extends BaseTest{
+public class GroupTest extends testBase{//BaseTest
 	
 	@Test
 	public void grouptest() throws InterruptedException {
